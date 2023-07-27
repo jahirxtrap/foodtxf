654f4f4bfb47503fa17c23660d52199431327287
jahirxtrap <jahirroman2002@gmail.com>
2023-07-26 20:27:45 -0500

    Custom patchouli book & baguette

--- src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
+++ src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
@@ -8,13 +8,10 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod("foodtxf")
+@Mod(FoodtxfMod.MODID)
 public class FoodtxfMod {
     public static final String MODID = "foodtxf";
-    public static final Logger LOGGER = LogManager.getLogger(FoodtxfMod.class);
 
     public FoodtxfMod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
