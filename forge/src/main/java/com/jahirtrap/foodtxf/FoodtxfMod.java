37c973bbd8dab7a38f71a58ddf2eab7c814c6a30
jahirxtrap <jahirroman2002@gmail.com>
2023-10-10 01:55:47 -0500

    Update config lib, fix better combat

--- forge/src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
+++ forge/src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
@@ -3,10 +3,11 @@ package com.jahirtrap.foodtxf;
 import com.jahirtrap.foodtxf.init.FoodtxfModBlocks;
 import com.jahirtrap.foodtxf.init.FoodtxfModConfig;
 import com.jahirtrap.foodtxf.init.FoodtxfModItems;
+import com.jahirtrap.foodtxf.util.configlib.TXFConfig;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FoodtxfMod.MODID)
@@ -16,8 +17,11 @@ public class FoodtxfMod {
     public FoodtxfMod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModLoadingContext.get().registerConfig(Type.COMMON, FoodtxfModConfig.COMMON_CONFIG);
+        TXFConfig.init(MODID, FoodtxfModConfig.class);
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
 
+        FoodtxfModTab.init();
         FoodtxfModBlocks.REGISTRY.register(bus);
         FoodtxfModItems.REGISTRY.register(bus);
         FoodtxfModTab.init();
