a5572ab1337e6b21db8958c5b185531db3d22297
jahirxtrap <jahirroman2002@gmail.com>
2023-12-07 02:05:13 -0500

    Update environment

--- src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
+++ src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
@@ -1,9 +1,9 @@
 package com.jahirtrap.foodtxf;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.foodtxf.init.FoodtxfModBlocks;
 import com.jahirtrap.foodtxf.init.FoodtxfModConfig;
 import com.jahirtrap.foodtxf.init.FoodtxfModItems;
-import com.jahirtrap.foodtxf.util.configlib.TXFConfig;
 import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -12,6 +12,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FoodtxfMod.MODID)
 public class FoodtxfMod {
+
     public static final String MODID = "foodtxf";
 
     public FoodtxfMod() {
@@ -21,8 +22,8 @@ public class FoodtxfMod {
         ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () ->
                 new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
 
-        FoodtxfModTab.init();
         FoodtxfModBlocks.REGISTRY.register(bus);
         FoodtxfModItems.REGISTRY.register(bus);
+        FoodtxfModTab.init();
     }
 }
