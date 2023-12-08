4a61edc5b1e98f7b823e60d5380deb0ff0fdc644
jahirxtrap <jahirroman2002@gmail.com>
2023-12-08 03:45:02 -0500

    Update dependencies

--- src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
+++ src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
@@ -1,9 +1,9 @@
 package com.jahirtrap.foodtxf;
 
-import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.foodtxf.init.FoodtxfModBlocks;
 import com.jahirtrap.foodtxf.init.FoodtxfModConfig;
 import com.jahirtrap.foodtxf.init.FoodtxfModItems;
+import com.jahirtrap.foodtxf.util.configlib.TXFConfig;
 import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
