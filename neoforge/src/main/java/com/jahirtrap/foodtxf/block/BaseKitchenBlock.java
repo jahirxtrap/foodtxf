8de76b6c41a66e0a90232912f2f3eb4b2cdbf91a
jahirxtrap <jahirroman2002@gmail.com>
2024-05-04 16:50:02 -0500

    Fixes

--- neoforge/src/main/java/com/jahirtrap/foodtxf/block/BaseKitchenBlock.java
+++ neoforge/src/main/java/com/jahirtrap/foodtxf/block/BaseKitchenBlock.java
@@ -4,7 +4,7 @@ import com.jahirtrap.foodtxf.util.TextUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
@@ -21,7 +21,7 @@ public class BaseKitchenBlock extends Block {
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, Item.TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag flag) {
+    public void appendHoverText(ItemStack stack, TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag flag) {
         if (ModList.get().isLoaded("cookingforblockheads")) {
             tooltip.add(TextUtils.coloredTextComponent("tooltip.cookingforblockheads:multiblock_kitchen", ChatFormatting.YELLOW));
             for (String s : I18n.get("tooltip.cookingforblockheads:kitchen_floor.description").split("\\\\n")) {
