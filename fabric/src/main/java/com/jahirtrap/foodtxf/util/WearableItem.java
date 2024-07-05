f4c71a3ded003eedbb83bd5bde47785e2a296ea3
jahirxtrap <jahirroman2002@gmail.com>
2024-07-05 17:33:10 -0500

    Remove lamp

--- fabric/src/main/java/com/jahirtrap/foodtxf/util/WearableItem.java
+++
@@ -1,8 +0,0 @@
-package com.jahirtrap.foodtxf.util;
-
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemStack;
-
-public interface WearableItem {
-    EquipmentSlot getEquipmentSlot(ItemStack stack);
-}
