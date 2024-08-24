cdd4f5165826ad2bac61bcb0c2c3515c24c39146
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:30:55 -0500

    Update config lib

--- neoforge/src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
+++ neoforge/src/main/java/com/jahirtrap/foodtxf/FoodtxfMod.java
@@ -1,13 +1,11 @@
 package com.jahirtrap.foodtxf;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.foodtxf.init.ModConfig;
 import com.jahirtrap.foodtxf.init.ModContent;
 import com.jahirtrap.foodtxf.init.ModTab;
-import com.jahirtrap.foodtxf.util.configlib.TXFConfig;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(FoodtxfMod.MODID)
 public class FoodtxfMod {
@@ -16,9 +14,6 @@ public class FoodtxfMod {
 
     public FoodtxfMod(IEventBus bus) {
         TXFConfig.init(MODID, ModConfig.class);
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
-
         ModContent.init(bus);
         ModTab.init(bus);
     }
