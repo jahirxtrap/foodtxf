649c482f697c37be41fa6bd34d4e04aa5d3c750e
jahirxtrap <jahirroman2002@gmail.com>
2024-12-23 23:46:28 -0500

    Rice, compostables, fuels, internal changes

--- forge/src/main/java/com/jahirtrap/foodtxf/init/ModTab.java
+++ forge/src/main/java/com/jahirtrap/foodtxf/init/ModTab.java
@@ -18,7 +18,7 @@ public class ModTab {
             .icon(() -> new ItemStack(ModContent.NETHERITE_SKILLET.get()))
             .displayItems((features, event) -> {
                 for (RegistryObject<Item> item : ModContent.ITEMS.getEntries())
-                    if (!ModContent.EXCLUDE_ITEMS.contains(item)) event.accept(item.get());
+                    if (!ModContent.EXCLUDED_ITEMS.contains(item)) event.accept(item.get());
             })
             .title(Component.translatable("itemGroup.foodtxf.tab_foodtxf"))
             .build());
