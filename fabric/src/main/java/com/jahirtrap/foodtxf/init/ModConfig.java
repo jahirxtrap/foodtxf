97f1b9eac95ad6c8bda6f69ee728c482a5b6fdf1
jahirxtrap <jahirroman2002@gmail.com>
2024-12-28 02:52:31 -0500

    v1.8.1

--- fabric/src/main/java/com/jahirtrap/foodtxf/init/ModConfig.java
+++ fabric/src/main/java/com/jahirtrap/foodtxf/init/ModConfig.java
@@ -3,10 +3,12 @@ package com.jahirtrap.foodtxf.init;
 import com.jahirtrap.configlib.TXFConfig;
 
 public class ModConfig extends TXFConfig {
-    @Entry(name = "Enable Cannibalism")
+    @Entry(name = "Enable Cannibalism", itemDisplay = "foodtxf:player_flesh")
     public static boolean enableCannibalism = true;
-    @Entry(name = "Player Drop Flesh")
+    @Entry(name = "Player Drop Flesh", itemDisplay = "foodtxf:player_flesh")
     public static boolean playerDropFlesh = true;
-    @Entry(name = "Knife Drop Flesh")
+    @Entry(name = "Knife Drop Flesh", itemDisplay = "foodtxf:iron_knife")
     public static boolean knifeDropFlesh = true;
+    @Entry(name = "Add Villager Trades", itemDisplay = "minecraft:emerald")
+    public static boolean addVillagerTrades = true;
 }
