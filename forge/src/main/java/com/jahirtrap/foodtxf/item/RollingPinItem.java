eb5ecdb6823a926cacd31fd647d4790939df2b5c
jahirxtrap <jahirroman2002@gmail.com>
2025-02-03 16:27:25 -0500

    Kuro materials compat, fix reusable items

--- forge/src/main/java/com/jahirtrap/foodtxf/item/RollingPinItem.java
+++ forge/src/main/java/com/jahirtrap/foodtxf/item/RollingPinItem.java
@@ -4,12 +4,11 @@ import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 
-public class RollingPinItem extends Item {
+public class RollingPinItem extends BaseReusableItem {
     public RollingPinItem() {
-        super(new Item.Properties().stacksTo(1).attributes(createAttributes()));
+        super(new Item.Properties().attributes(createAttributes()));
     }
 
     public static ItemAttributeModifiers createAttributes() {
@@ -19,15 +18,5 @@ public class RollingPinItem extends Item {
                 .add(Attributes.ATTACK_SPEED,
                         new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.8F, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .build();
-    }
-
-    @Override
-    public boolean hasCraftingRemainingItem() {
-        return true;
-    }
-
-    @Override
-    public ItemStack getCraftingRemainingItem(ItemStack stack) {
-        return stack;
     }
 }
