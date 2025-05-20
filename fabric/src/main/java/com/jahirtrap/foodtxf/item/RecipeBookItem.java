96eb631eac1f73370606ef8a10b3ffe42d5d432d
jahirxtrap <jahirroman2002@gmail.com>
2025-05-20 13:23:26 -0500

    Modonomicon recipe book

--- fabric/src/main/java/com/jahirtrap/foodtxf/item/RecipeBookItem.java
+++ fabric/src/main/java/com/jahirtrap/foodtxf/item/RecipeBookItem.java
@@ -1,10 +1,11 @@
 package com.jahirtrap.foodtxf.item;
 
+import com.klikli_dev.modonomicon.client.gui.BookGuiManager;
+import com.klikli_dev.modonomicon.client.gui.book.BookAddress;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -12,7 +13,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import vazkii.patchouli.api.PatchouliAPI;
 
 import java.util.List;
 
@@ -23,19 +23,16 @@ public class RecipeBookItem extends Item {
 
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
-        if (player instanceof ServerPlayer serverPlayer && FabricLoader.getInstance().isModLoaded("patchouli"))
-            PatchouliAPI.get().openBookGUI(serverPlayer, BuiltInRegistries.ITEM.getKey(this));
-        else return super.use(level, player, hand);
-
-        return InteractionResult.SUCCESS;
+        if (FabricLoader.getInstance().isModLoaded("modonomicon")) {
+            BookGuiManager.get().openBook(BookAddress.defaultFor(BuiltInRegistries.ITEM.getKey(this)));
+            return InteractionResult.SUCCESS;
+        } else return super.use(level, player, hand);
     }
 
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag tooltipFlag) {
-        if (!FabricLoader.getInstance().isModLoaded("patchouli")) {
-            tooltip.add(Component.translatable("tooltip.foodtxf.patchouli").withStyle(ChatFormatting.GRAY));
-        } else {
-            tooltip.add(Component.translatable("patchouli.gui.lexicon.edition_str", "1st").withStyle(ChatFormatting.GRAY));
+        if (!FabricLoader.getInstance().isModLoaded("modonomicon")) {
+            tooltip.add(Component.translatable("tooltip.foodtxf.modonomicon").withStyle(ChatFormatting.GRAY));
         }
     }
 }
